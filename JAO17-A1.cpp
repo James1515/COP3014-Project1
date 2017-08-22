eaff9b58e31d14dd439a4729d56c63066428c328
James Ortiz <James1515@users.noreply.github.com>
2017-08-21 23:53:45 -0400

    Add files via upload

---
+++ JAO17-A1.cpp
@@ -0,0 +1,13 @@
+// ***********************
+// James Anthony Ortiz
+// Assignment #1
+// ***********************
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World\n";
+	std::cout << "My name is James Anthony Ortiz" << std::endl;
+	std::cin.get();
+}
